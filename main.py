f1d38a4f1d27c9d54d4254c3fc010ba182fe2417
jeyyoon <jeyyoon.ru@nuvi-labs.com>
2021-10-07 17:13:53 +0900

    parser

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ import os.path as osp
 import glob
 from inference import Nuvi_RecycleNet
 
+parser = argparse.ArgumentParser(
+                    description='Nuvilabs RecycleNet')
+parser.add_argument('--config_file', default='./model/model_config.py', type=str,
+                    help='Model configuration file path.')
+parser.add_argument('--checkpoint_file', default='./model/model_checkpoint.pth', type=str,
+                    help='Model checkpoint file path.')
+parser.add_argument('--img_path', default='./sample_img.jpg', type=str,
+                    help='Path of image or images.')
+parser.add_argument('--threshold', default=0.6, type=float,
+                    help='Only boxes with the score larger than this will be detected.')
+parser.add_argument('--use_tta', dest='use_tta', action='store_true',
+                    help='Either use TTA to help detect images.')
+parser.set_defaults(use_tta=False)
