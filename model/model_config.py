c2d2edef7aef21cfc4aa7f0ecbdb510da36ee501
donghyun999 <donghyun.kim@nuvi-labs.com>
2021-10-07 18:30:31 +0900

    update model backbone

---
+++ model/model_config.py
@@ -0,0 +1,18 @@
+model = dict(
+    type='CascadeRCNN',
+    pretrained='torchvision://resnet50',
+    backbone=dict(
+        type='DetectoRS_ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch',
+        conv_cfg=dict(type='ConvAWS'),
+        sac=dict(type='SAC', use_deform=True),
+        stage_with_sac=(False, True, True, True),
+        output_img=True),
+    neck=dict(),
+    rpn_head=dict()
