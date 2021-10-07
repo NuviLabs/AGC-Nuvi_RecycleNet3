dacb336912bbf25cb512d1c247bd2f92160d5f4f
sehwanj <sehwan.joo@nuvi-labs.com>
2021-10-07 17:26:34 +0900

    define nuvi_recyclenet

--- inference.py
+++ inference.py
@@ -17,4 +17,50 @@ from mmcv.ops import RoIPool
 from mmcv.parallel import collate, scatter
 
 
+class Nuvi_RecycleNet():
+    def __init__(self,
+                 config_file,
+                 checkpoint_file,
+                 threshold,
+                 device='cuda:0',
+                 tta=True):
 
+        self.config_file = config_file
+        self.config_drs = Config.fromfile(config_file)
+        self.checkpoint_file = checkpoint_file
+        self.threshold = threshold  # Only boxes with the score larger than this will be detected
+        self.tta = tta # Perform TTA on not detected images
+
+        # Build the model from a config file and a checkpoint file
+        # self.model = init_detector(self.config_file, self.checkpoint_file, device=device)
+        self.model = torch.load(self.checkpoint_file)
+        self.model.eval()
+        self.model.cuda()
+
+
+        print('Model loaded sucessfully. Ready to perform inference.')
+        print('Threshold: ', self.threshold)
+        self.classes = {0: 'paper',
+                        1: 'paper_pack',
+                        2: 'steel',
+                        3: 'glass',
+                        4: 'PET',
+                        5: 'plastic',
+                        6: 'plasticbag'
+                        }
+
+    def augmentIMG(self, img_array, augment_type):
+        if augment_type == 'LR':
+            print('augmented', augment_type)
+            img = np.fliplr(img_array)
+
+        elif augment_type == 'UDR':
+            print('augmented', augment_type)
+            img = np.flipud(img_array)
+
+        elif augment_type == 'RT':
+            print('augmented', augment_type)
+            img = np.rot90(img_array)
+
+        return img
+    
