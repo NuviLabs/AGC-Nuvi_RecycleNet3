48f81352a0c4465787966632835c13abe9458229
jeyyoon <jeyyoon.ru@nuvi-labs.com>
2021-10-07 17:13:26 +0900

    Import packages

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: Nuvilabs - Luca Medeiros, luca.medeiros@nuvi-labs.com
+"""
+
+import argparse
+import os.path as osp
+import glob
+from inference import Nuvi_RecycleNet
+
